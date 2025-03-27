9e2edf4f2d43425ef5175b5776392d07757b063d
Reet Khanchandani <khanchandani.reet@gmail.com>
2025-03-26 21:43:34 -0700

    Deploying..

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/reet-571099/Portfolio'
 })
+
+
+
+
